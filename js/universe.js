3aab93cd307b31c46266c966418022e5d4776a50
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-03-01 15:29:18 +0600

    finish the details sections

--- js/universe.js
+++ js/universe.js
@@ -12,7 +12,6 @@ const displayData = (allData, isLoaded) => {
     }
     cartContainer.innerHTML = '';
     allData.forEach(data =>{
-        console.log(data.id);
      const div = document.createElement('div');
     div.classList = `card card-compact w-96 bg-gray-400 shadow-xl`;
     div.innerHTML = `
@@ -27,7 +26,7 @@ const displayData = (allData, isLoaded) => {
                   <h3 class="text-[25px] font-semibold my-2">${data.name}</h3>
                   <p>Published date:${data.published_in}</p>
                   <div class="card-actions justify-end">
-                  <button onclick='displayDetails("${data.id}")' class="btn btn-primary">SEE DETAILS</button>
+                  <button onclick='detailInfo("${data.id}")' class="btn btn-primary">SEE DETAILS</button>
                 </div>
                   </div>
                 </div>
@@ -45,10 +44,25 @@ const detailInfo = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
     const dataId= await res.json();
     displayDetails(dataId)
+    // console.log(dataId.data);
     }
     // show details information //
     const displayDetails = (dataId) =>{
-        console.log(dataId.data);
-        detailInfo();
+        console.log(dataId.data.features[1].feature_name);
+        const detailContainer = document.getElementById('details-container');
+        detailContainer.innerHTML = `
+        <img src="${dataId.data.image_link}" alt="see details">
+        <h3 class="font-bold text-2xl">${dataId?.data?.tool_name}</h3>
+        <h3 class="font-semibold text-lg">${dataId?.data?.description}</h3>
+        <h2 class="card-title">Features</h2>
+                  <p>1.${dataId.data.features[1].feature_name}</p>
+                  <p>2.${dataId.data.features[2].feature_name}</p>
+                  <p>3.${dataId.data.features[3].feature_name}</p>
+                  
+                  
+                  
+        `
+
+        details_modal.showModal()
     }
 dataLoader(false);
